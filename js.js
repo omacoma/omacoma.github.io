5ad80551db4ca042b3e957165aa58fc78be78c28
Омаров Темирлан < >
2016-11-29 11:20:59 +0600

    first commit

---
+++ js.js
@@ -0,0 +1,24 @@
+window.onload = function(){
+	
+	var Red = document.getElementById('Red');
+	var Green = document.getElementById('Green');
+	var Blue = document.getElementById('Blue');
+	var Fahriddin = document.getElementById('Orange');
+	Red.onclick = function(){
+		document.body.style.background = Red.id;
+
+}
+	Green.onclick = function(){
+		document.body.style.background = Green.id;
+	}
+
+	Blue.onclick = function(){
+		document.body.style.background = Blue.id;
+
+	}
+	Orange.onclick = function(){
+		document.body.style.background = Orange.id;
+
+	}
+};
+
